0cee1483d941194ea53345dc40ffc6583faa29e8
MultiFlex <wakanawardrobe@gmail.com>
2022-03-11 17:23:52 +0000

    add else and if statement for one round game

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
 
-function counterPlay() {
+function computerPlay() {
     let value = Math.floor(Math.random() * 3);
     if (value === 0) {
-        console.log("rock");
+        return "rock";
     } if (value === 1) {
-        console.log("paper");
+        return "paper";
     } else if (value === 2) {
-        console.log("scissors");
+        return "scissors";
     }
 }
 
-counterPlay()
+function playRound(playerSelection,computerSelection) {
+    if (playerSelection === "rock" && computerSelection === "rock") {
+        console.log("Its a draw!");
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        console.log("You lose! paper beats rock");
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        console.log("Player has won! Rock beats scissors");
+    } 
+    
+    if (playerSelection === "paper" && computerSelection === "paper" ) {
+        console.log("Its a draw!");
+    } else if (playerSelection === "paper" && computerSelection === "rock" ) {
+        console.log("You have won! paper beats rock");
+    } else if (playerSelection === "paper" && computerSelection === "scissors" ) {
+        console.log("You lose! Scissors beats paper");
+    } 
+    
+    if (playerSelection === "scissors" && computerSelection === "scissors") {
+        console.log("Its a draw!");
+    } else if (playerSelection === "scissors" && computerSelection === "paper" ) {
+        console.log("You win! scissors beats paper");
+    } else if (playerSelection === "scissors" && computerSelection === "rock" ) {
+        console.log("You lose! Rock beats scissors");
+    }
+}
+
+const playerSelection = "paper";
+const computerSelection = computerPlay();
+
+console.log(playRound(playerSelection, computerSelection));
