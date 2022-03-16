c0558476161488e551678b1010c50a169c33c6e7
MultiFlex <wakanawardrobe@gmail.com>
2022-03-16 12:51:47 +0000

    add option to cancel the game

--- script.js
+++ script.js
@@ -7,11 +7,12 @@ function computerChoice() {
 //lets the user pick a valid option
 function playerChoice() {
     let playerInput = prompt("Rock,Paper or Scissors?");
-    if (playerInput == null || undefined) {
-        alert ("You canceled the game");
+    if (playerInput == null || playerInput == undefined) {
+        //alert ("You canceled the game");
+        return "cancel";
     } 
     let playerInputValid = playerInput.toLowerCase();
-    if (playerInputValid == "rock" || "paper" || "scissors") {
+    if (playerInputValid == "rock" || playerInputValid == "paper" || playerInputValid == "scissors") {
         return playerInputValid;
     } else {
         alert ("That is not a valid answer");
@@ -19,10 +20,14 @@ function playerChoice() {
         return alternateAnswer;
     }
 }
+
 //plays one round of the game
 function playOneRound() {
     let playerAnswer = playerChoice();
     let computerAnswer = computerChoice();
+    if (playerAnswer == "cancel") {
+        return "cancel";
+    } else {
     console.log(`You picked ${playerAnswer} and the computer picked ${computerAnswer}`)
     if (playerAnswer == computerAnswer) {
         return "draw";
@@ -41,11 +46,17 @@ function playOneRound() {
     } else if (playerAnswer == "rock" && computerAnswer == "paper") {
         return "lose";
     }
+    }
 }
 //plays 5 rounds of the game using a loop and keeps score
 function game() {
+    //let letsGame = prompt("Shall we play a best of five?")
     let playerScore = 0;
     let computerScore = 0;
+    let roundCancel = playOneRound();
+    if (roundCancel == "cancel") {
+        alert("You canceled the game");
+    } else {
     for (i = 0; i < 5; i++) {
         let round = playOneRound()
         if (round == "win") {
@@ -56,8 +67,11 @@ function game() {
             console.log("It was a draw!");
         }
     }
+        
     //returns the result at the end
     return `The game is over. You scored ${playerScore} and the computer scored ${computerScore}`;
+    }
+    return "Don't cancel next time! Refresh the page is you want to play"
 }
 
 console.log(game());
