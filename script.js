d8ed674aa0eb0027879ef1dd6bd7d3733f0ab901
MultiFlex <wakanawardrobe@gmail.com>
2022-04-05 12:21:12 +0100

    add dragable main container

--- script.js
+++ script.js
@@ -19,10 +19,44 @@ const mainDiv = document.querySelector('.result');
 let result = document.querySelector('#answer');
 const gameOver = document.querySelector('#game-over');
 
+const mainBox = document.querySelector('.container');
+
 const livePlayerScore = document.querySelector('#player-score');
 const liveComputerScore = document.querySelector('#computer-score');
 
+mainBox.addEventListener('mousedown', mousedown);
+
+function mousedown(e) {
+    window.addEventListener('mousemove', mousemove);
+    window.addEventListener('mouseup', mouseup);
+
+    let prevX = e.clientX;
+    let prevY = e.clientY;
+
+    function mousemove(e) {
+        let newX = prevX - e.clientX;
+        let newY = prevY - e.clientY;
+
+        const rect = mainBox.getBoundingClientRect();
+
+        mainBox.style.left = rect.left - newX + "px";
+        mainBox.style.top = rect.top - newY + "px";
+
+        prevX = e.clientX;
+        prevY = e.clientY;
+    }
+
+    function mouseup () {
+        window.removeEventListener('mousemove', mousemove);
+        window.removeEventListener('mouseup', mouseup);
+
+    }
+}
+
 function resetState() {
+    if (total < 5 ) {
+        return;
+    } else {
     mainDiv.removeChild(result);
     gameOver.classList.remove('win');
     gameOver.classList.remove('lose');
@@ -30,6 +64,7 @@ function resetState() {
     playerScore = 0;
     computerScore = 0;
     total = 0;
+    }
 }
 
 function fiveRounds(e) {
@@ -60,7 +95,6 @@ function fiveRounds(e) {
             }
             mainDiv.appendChild(gameOver);
             gameOver.textContent = `The game is over. You scored ${playerScore} and the computer scored ${computerScore}`;
-            // console.log(`The game is over. You scored ${playerScore} and the computer scored ${computerScore}`);
         }
 }
 
@@ -76,37 +110,13 @@ function fiveRounds(e) {
     });
     }
 
-
-
-    function gameStart() {
-        const playGame = document.getElementById('play-game');
-
-        playGame.classList.add('play-game');
-    }
-
-
-    
-    // let playerInput = prompt("Rock,Paper or Scissors?");
-    // if (playerInput == null || playerInput == undefined) {
-    //     return "cancel";
-    // } 
-    // let playerInputValid = playerInput.toLowerCase();
-    // if (playerInputValid == "rock" || playerInputValid == "paper" || playerInputValid == "scissors") {
-    //     return playerInputValid;
-    // } else {
-    //     alert ("That is not a valid answer");
-    //     let alternateAnswer = playerChoice();
-    //     return alternateAnswer;
-    // }
-//}
-
-//plays one round of the game
 function playOneRound(answer) {
     let playerAnswer = answer;
     let computerAnswer = computerChoice();
+
     result.textContent = `You picked ${playerAnswer} and the computer picked ${computerAnswer}!`;
     mainDiv.appendChild(result);
-    // console.log(`You picked ${playerAnswer} and the computer picked ${computerAnswer}`)
+
     if (playerAnswer == computerAnswer) {
         return "draw";
     } else if (playerAnswer == "paper" && computerAnswer == "rock") {
@@ -125,46 +135,6 @@ function playOneRound(answer) {
         return "lose";
     }
 
-}
-
-//plays 5 rounds of the game using a loop and keeps score
-// function game() {
-//     let letsGame = prompt("How many rounds do you want to play?");
-//     let roundNumber = parseInt(letsGame);
-//     if (Number.isInteger(roundNumber)) {
-//     let playerScore = 0;
-//     let computerScore = 0;
-//     for (i = 0; i < roundNumber; i++) {
-//         let round = playOneRound()
-//         if (round == "win") {
-//             playerScore++; console.log("You win!");
-//         } else if (round == "lose") {
-//             computerScore++; console.log("You lose!");
-//         } else if (round == "draw") {
-//             console.log("It was a draw!");
-//         } else if (round == "cancel") {
-//             return "you canceled the game";
-//         }
-//     }
-//     //returns the result at the end
-//     console.log(`The game is over. You scored ${playerScore} and the computer scored ${computerScore}`);
-//     let gameAgain = playAgain();
-//     return gameAgain;
-//     } else if (letsGame == undefined) {
-//         return "Canceled. You should play at least a few rounds :(";
-//     } else if (isNaN(roundNumber)) {
-//         console.log("Pick a number next time!")
-//         return game();
-//     }
-// }
-
-function playAgain() {
-    let gameAgain = confirm("Would you like to play again?")
-    if (gameAgain == true) {
-        return game();
-    } else if (gameAgain == false) {
-        return "Okay! See you next time!";
-    }
 }
 
 
